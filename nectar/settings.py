492bef3e6e2516ee9591b2abe1d41b23e99e5dfa
musaib-js <musaibaltafurahman@gmail.com>
2022-12-15 22:50:32 +0530

    commit

--- nectar/settings.py
+++ nectar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nectar-infotech.herokuapp.com', 'nectarinfotech.in', 'www.nectarinfotech.in', '127.0.0.0.1']
+ALLOWED_HOSTS = ['nectar-infotech.herokuapp.com', 'nectarinfotech.in', 'www.nectarinfotech.in', '127.0.0.0.1:8000']
 #ALLOWED_HOSTS = []
 
 
