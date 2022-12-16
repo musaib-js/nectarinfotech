a51a3e86ccff4e5eb43dc34a1582df274dd3acf2
musaib-js <musaibaltafurahman@gmail.com>
2022-12-16 12:18:39 +0530

    404 URL

--- nectar/urls.py
+++ nectar/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+handler404 = 'home.views.error_404_view'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
